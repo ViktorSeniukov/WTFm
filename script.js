7bb4fcd92dfa6dff628e03201b43a36ce9518138
Viktor Seniukov <spinnekopser@gmail.com>
2022-11-15 16:00:27 +0300

    Add function axios

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+// const { default: axios } = require("axios");
+
+// const { createElement } = require("react");
+
 // let btnCircle = document.getElementsByClassName("cricle");
 const btn = document.getElementById("button");
 let form = document.querySelector('#form');
 let cardList = document.querySelector(".cards-list");
-let cardItemAll = document.querySelectorAll(".card-item");
+let newCardName = document.querySelector("#input-name");
 let newCardTitle = document.querySelector("#input-title");
 let newCardText = document.querySelector("#input-text");
 
@@ -13,16 +17,30 @@ let newCardText = document.querySelector("#input-text");
 
 function addCard(event) {
   event.preventDefault();
-  console.log(cardItemAll);
+  let cardItemAll = document.querySelectorAll(".card-item");
   let firstCardItem = cardItemAll[0];
-  console.log(firstCardItem);
   let cardItem = document.createElement('li');
   cardItem.className = 'card-item';
+  let cardItemName = newCardName.value;
   let cardItemTitle = newCardTitle.value;
   let cardItemText = newCardText.value;
-  cardItem.innerHTML = '<h2 class="card-title">'+cardItemTitle+'</h2> <p class="card-text">'+cardItemText+'</p>';
+  cardItem.innerHTML = '<p class="card-name">'+cardItemName+'</p> <h2 class="card-title">'+cardItemTitle+'</h2> <p class="card-text">'+cardItemText+'</p>';
   cardList.insertBefore(cardItem, firstCardItem);
-  console.log(cardItemAll[0]);
+
+  let url = 'https://sheet.best/api/sheets/2c798965-3ead-48a5-aa4f-cf29b2a39e3d';
+  console.log(url);
+
+  axios.post(url, {
+    name: cardItemName,
+    title: cardItemTitle,
+    text: cardItemText
+  })
+  .then(function (ressponse) {
+    console.log(ressponse);
+  })
+  .then(function (error) {
+    console.log(error);
+  });
 }
 
 function validation() {
@@ -33,6 +51,35 @@ function validation() {
     btn.setAttribute('disabled', '');
   }
 }
+
+let url = 'https://sheet.best/api/sheets/2c798965-3ead-48a5-aa4f-cf29b2a39e3d';
+console.log(url);
+
+axios.get(url).then(res => {
+  console.log(res.data);
+
+  let dataLength = res.data.length;
+  console.log(dataLength);
+
+  for (let i = 0; i < res.data.length; i++) {
+    let cardItemAll = document.querySelectorAll(".card-item");
+    let lastItem = document.createElement('li');
+    lastItem.className = 'card-item'
+    let lastItemName = res.data[i].name;
+    let lastItemTitle = res.data[i].title;
+    let lastItemText = res.data[i].text;
+    console.log(lastItemName, lastItemTitle, lastItemText);
+    lastItem.innerHTML = '<p class="card-name">'+lastItemName+'</p> <h2 class="card-title">'+lastItemTitle+'</h2> <p class="card-text">'+lastItemText+'</p>';
+    let firstTwoCardItem = cardItemAll[0];
+    cardList.insertBefore(lastItem, firstTwoCardItem);
+  }
+})
+.then(function (ressponse) {
+  console.log(ressponse);
+})
+.then(function (error) {
+  console.log(error);
+});
 
 newCardTitle.addEventListener('input', validation);
 form.addEventListener("submit", addCard);
